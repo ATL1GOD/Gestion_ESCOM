c901e84fee3723ea2ffd90bf1ab8769d1dc9df15
Marian Guzman <147306242+MarianGuzman1829@users.noreply.github.com>
2025-08-05 23:42:41 -0600

    Rotation lock
    Forced vertical position added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 // import 'package:device_preview/device_preview.dart';
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(
     // DevicePreview(
     //   enabled: true,
